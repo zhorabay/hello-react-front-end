8352941b0616930cd88da9ee0385b20b1b1bae5a
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-01-16 19:16:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
